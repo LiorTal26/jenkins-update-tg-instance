1e4539ce35ce76d68e02a3eff3e1142de7fd418d
LiorTal26 <lior.tal13@gmail.com>
2025-04-28 11:26:29 +0300

    cred

--- main.tf
+++ main.tf
@@ -1,17 +1,67 @@
-resource "aws_launch_template" "lior" {
-  name_prefix   = "asg-lior-terra"
-  image_id      = "ami-0ae7e1e8fb8251940"
+provider "aws" {
+  region = "il-central-1"
+}
+data "aws_subnets" "private" {
+  
+  filter {
+    name   = "availability-zone"
+    values = ["il-central-1a", "il-central-1b"]
+  }
+
+}
+locals{
+  ami_id = "ami-0ae7e1e8fb8251940"
+  subnets = [
+    "subnet-01e6348062924d048",
+    "subnet-0a1cbd99dd27a5307",
+    "subnet-2p3q4r5s6t7u8v9w0",
+    "subnet-3x4y5z6a7b8c9d0e1"
+  ]
+}
+
+data "aws_lb_target_group" "app_tg" {
+  name = "tg-lior"
+}
+
+# single launch_template for all ASG
+resource "aws_launch_template" "example" {
+  name_prefix   = "asg-lior-terraform-"
+  image_id      = local.ami_id
   instance_type = "t2.micro"
 }
 
-resource "aws_autoscaling_group" "bar" {
-  availability_zones = ["il-central-1a", "il-central-1b"]
-  desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
+# create number of ASG with index counter name
+resource "aws_autoscaling_group" "lior" {
+  count                = 3
+  desired_capacity     = 1
+  max_size             = 2
+  min_size             = 1
+  vpc_zone_identifier  = local.subnets
+  health_check_type    = "EC2"
+  name               = "asg-lior-terraform-${count.index + 1}"
 
   launch_template {
-    id      = aws_launch_template.foobar.id
+    id      = aws_launch_template.example.id
     version = "$Latest"
   }
+
+  tag {
+    key                 = "Name"
+    value               = "lior-${count.index +1}"
+    propagate_at_launch = true
+  }
+}
+
+output "launch_template_id" {
+  value = aws_launch_template.example.id
+}
+
+output "asg_names"{
+  description = "List of ASG names"
+  value      = aws_autoscaling_group.lior[*].name
+}
+
+output "tg_arn" {
+  description = "ARN of ALB Target Group by name"
+  value      = data.aws_lb_target_group.app_tg.arn
 }
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+public_subnet_ids = [
+  "subnet-0ab01234cdef5678a",  # il-central-1a
+  "subnet-0bc12345def6789ab"   # il-central-1b
+]
+
+aws_region = "il-central-1"
